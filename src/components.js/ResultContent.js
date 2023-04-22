3e673e99b6351b42845c2dedec4e098f55428f29
unknown <taiduong.264@gmail.com>
2023-04-22 16:43:10 +0700

    submit

--- src/components.js/ResultContent.js
+++ src/components.js/ResultContent.js
@@ -28,7 +28,7 @@ const ResultContent = () => {
         const tld = nation.tld
         const population = nation.population
         const timezones = nation.timezones
-        const startOfWeek =  nation.startOfWeek
+        const startOfWeek = nation.startOfWeek
         const idd = nation.idd
 
 
@@ -65,8 +65,8 @@ const ResultContent = () => {
                 Object.keys(nativeName).map((key, index) =>
                 (
                     <div key={index} className={`w-full flex flex-col gap-2 my-2`}>
-                        <p className={`text-3xl m-auto md:m-0 font-bold ${color_theme}`}>{nativeName[key].official}</p>
-                        <span className={`text-2xl m-auto md:m-0 font-bold ${color_theme}`}>( {nativeName[key].common} )</span>
+                        <p className={`text-2xl m-auto md:m-0 font-bold ${color_theme}`}>{nativeName[key].official}</p>
+                        <span className={`text-xl m-auto md:m-0 font-bold ${color_theme}`}>( {nativeName[key].common} )</span>
                     </div>
                 )
                 )
@@ -89,6 +89,7 @@ const ResultContent = () => {
         return (
             <>
                 <div className={`w-full h-fit flex flex-row gap-2 p-2 bg-opacity-80 md:bg-opacity-100 rounded-md relative ${localDarkmode === DARK ? "out-box-effect bg-zinc-800" : "bg-slate-50 in-box-effect"}`}>
+
                     <img
                         className={`w-1/2 lg:w-1/3 lg:max-w-4xl object-contain`}
                         src={flag.svg}
@@ -101,22 +102,34 @@ const ResultContent = () => {
                 </div>
 
                 <div className={`w-full flex flex-col p-2 gap-2 bg-opacity-80 md:bg-opacity-100 rounded-md ${localDarkmode === DARK ? "out-box-effect bg-zinc-800" : "bg-slate-50 in-box-effect"}`}>
-
                     <RenderName />
-                    <div className={`w-full flex flex-col justify-start gap-2`}>
-                        <RenderInfo list={cap} topkey={"Capital"} />
-                        <RenderInfo list={languages} topkey={"Languages"} />
-                        <RenderInfo list={tld} topkey={"Tld of nation"} />
+                    <div className='w-full border rounded p-1'>
+                        <p className={`text-md italic mb-2 ${color_theme}`}>General:</p>
+
+                        <div className={`w-full flex flex-col justify-start gap-2 p-2`}>
+                            <p className={`text-md ${color_theme}`}>Offical name: <span className='font-bold'>{name.official}</span></p>
+                            <RenderInfo list={cap} topkey={"Capital"} />
+                            <p className={`text-md ${color_theme}`}>Population: <span className='font-bold'>{population}</span></p>
+                            <RenderInfo list={timezones} topkey={"Timezones"} />
+                            <p className={`text-md ${color_theme}`}>Region: <span className='font-bold'>{region} - {subregion}</span></p>
+
+                        </div>
                     </div>
-                    <RenderIdd />
-                    <RenderCurrency />
-                    <p className={`text-md ${color_theme}`}>Offical: <span className='font-bold'>{name.official}</span></p>
-                    <p className={`text-md ${color_theme}`}>Car side: <span className='font-bold'>{car.side.charAt(0).toUpperCase() +  car.side.slice(1)}</span></p>
-                    <p className={`text-md ${color_theme}`}>Region: <span className='font-bold'>{region} - {subregion}</span></p>
-                    <p className={`text-md ${color_theme}`}>Population: <span className='font-bold'>{population}</span></p>
-                    <RenderInfo list={timezones} topkey={"Timezones"} />
-                    
-                    <p className={`text-md ${color_theme}`}>Start of week: <span className='font-bold'>{startOfWeek.charAt(0).toUpperCase() +  startOfWeek.slice(1)}</span></p>
+                    <div className='w-full border rounded p-1'>
+                    <p className={`text-md italic mb-2 ${color_theme}`}>Other information:</p>
+
+                        <div className={`w-full flex flex-col justify-start gap-2 p-2 `}>
+                            <RenderIdd />
+                            <RenderInfo list={tld} topkey={"Tld of nation"} />
+                            <RenderCurrency />
+
+                            <RenderInfo list={languages} topkey={"Languages"} />
+                            <p className={`text-md ${color_theme}`}>Car side: <span className='font-bold'>{car.side.charAt(0).toUpperCase() + car.side.slice(1)}</span></p>
+
+
+                            <p className={`text-md ${color_theme}`}>Start of week: <span className='font-bold'>{startOfWeek.charAt(0).toUpperCase() + startOfWeek.slice(1)}</span></p>
+                        </div>
+                    </div>
                 </div>
             </>
         )
